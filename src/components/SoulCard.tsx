1d8f7787d7edb2a29e17aabfedf61393a708c21d
Rauf-ID <rauf.develop@gmail.com>
2026-05-11 20:43:42 +0500

    brief overview of frontend

---
+++ src/components/SoulCard.tsx
@@ -0,0 +1,23 @@
+import React, { type JSX } from 'react';
+
+interface SoulCardProps {
+  name: string
+  birthYear: number
+  deathYear: number
+}
+
+const SoulCard: React.FC<SoulCardProps> = ({ 
+    name, birthYear, deathYear 
+}): JSX.Element => {
+    return (
+        <div>
+        <h2>{name}</h2>
+
+        <p>
+            {birthYear} - {deathYear}
+        </p>
+        </div>
+    )
+}
+
+export default SoulCard;
